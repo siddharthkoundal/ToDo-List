a1ee0272c511797c50a01b3d0d12bbed116e562a
siddharthkoundal <sksiddharth27@gmail.com>
2022-01-20 18:52:42 +0530

    Initial Commit

---
+++ theme.js
@@ -0,0 +1,25 @@
+standardTheme.addEventListener('click', () => changeTheme('standard'));
+lightTheme.addEventListener('click', () => changeTheme('light'));
+darkerTheme.addEventListener('click', () => changeTheme('darker'));
+
+function setTheme(themeName){
+    localStorage.setItem("theme", themeName);
+    document.documentElement.className = themeName;
+}
+
+function toggleTheme(){
+    if(localStorage.getItem("theme") == "theme-dark"){
+        setTheme("theme-light");
+    }
+    else{
+        setTheme("theme-dark");
+    }
+}
+
+(function () {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-dark');
+    } else {
+        setTheme('theme-light');
+    }
+ })();
